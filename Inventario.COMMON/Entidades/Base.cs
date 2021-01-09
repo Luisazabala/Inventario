170e0b10b54c5fa77718f5223d8157671e546cc8
LuisaZabala <luisazabala99@gmail.com>
2021-01-09 12:42:32 -0500

    Agregar archivos de proyecto.

---
+++ Inventario.COMMON/Entidades/Base.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Inventario.COMMON.Entidades
+{
+    public abstract class Base
+    {
+        public string Id { get; set; }
+
+    }
+}
---
+++ Inventario.COMMON/Entidades/Empleado.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Inventario.COMMON.Entidades
+{
+    public class Empleado:Base
+    {
+        public string Id { get; set; }
+        public string Nombre { get; set; }
+        public string Apellidos { get; set; }
+        public string Area { get; set; }
+    }
+}
